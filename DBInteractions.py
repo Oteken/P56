156d1565b951ce958736717dd01c9506661b5db5
Oteken <Otekenpekince@hotmail.com>
2015-11-24 19:15:16 +0100

    Empty

--- DBInteractions.py
+++
@@ -1,9 +0,0 @@
-
-class DBInteractions:
-
-    def insertList(self, table, dataList, header):
-        for i in range(0, len(dataList)):
-            insertDict = {}
-            for j in range(0, len(header)):
-                insertDict[header[j]] = dataList[i][j]
-            table.insert(insertDict)
