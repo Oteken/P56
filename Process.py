1c5caf2dc2502d24c33e493ae320f9a4bf846794
Oteken <otekenpekince@hotmail.com>
2015-12-06 21:03:11 +0100

    Changed for server environment inserting.

--- Process.py
+++ Process.py
@@ -1,31 +1,115 @@
 import DBInteractions
 import CsvInteractions
 from pymongo import MongoClient
+import csv
+import datetime
 
 DB = DBInteractions.DBInteractions()
 CSV = CsvInteractions.CsvInteractions()
 
-uri = ('mongodb://oteken:lol123123@ds047484.mongolab.com:47484/citygis')
+uri = ('mongodb://localhost:27017/')
 client = MongoClient(uri)
 db = client.citygis
 
+connectionsCsvFilePath = 'C:/Users/0895642/Downloads/CSV Files/connections.csv'
+monitoringCsvFilePath = 'C:/Users/0895642/Downloads/CSV Files/monitoring.csv'
+eventsCsvFilePath = 'C:/Users/0895642/Downloads/CSV Files/events.csv'
+positionsCsvFilePath = 'C:/Users/0895642/Downloads/CSV Files/positions.csv'
+
+connectionsDataTypes = ["date", "string", "string", "int"]
+monitoringDataTypes = ["string", "date", "date", "string", "float", "float", "float"]
+eventsDataTypes = ["date", "string", "string", "string", "int"]
+positionsDataTypes = ["date", "string", "float", "float", "int", "int", "int", "int", "string"]
+
 connectionsTable = db.connections
-connectionsCsvFilePath = 'D:/Oteken/School/INF/Jaar 2/Periode 1/Project 5 6/connections.csv'
 monitoringTable = db.monitoring
-monitoringCsvFilePath = 'D:/Oteken/School/INF/Jaar 2/Periode 1/Project 5 6/monitoring.csv'
 eventsTable = db.events
-eventsCsvFilePath = 'D:/Oteken/School/INF/Jaar 2/Periode 1/Project 5 6/events.csv'
 positionsTable = db.positions
-positionsCsvFilePath = 'D:/Oteken/School/INF/Jaar 2/Periode 1/Project 5 6/positions.csv'
 
-def readAndInsert(filePath, table):
-    csvData = CSV.readCsvFile(filePath, ";")
-    csvHeader = CSV.readCsvHeader(filePath, ";")
-    DB.insertList(table, csvData, csvHeader)
+def fastInsert(filePath, delimiter, table, dataTypes):
+    with open(filePath) as csvfile:
+        reader = csv.reader(csvfile, delimiter=delimiter)
+        header = next(reader)
+        i = 0
+        insertDict = {}
+        for row in reader:
+            i = 0
+            insertDict = {}
+            for x in row:
 
-readAndInsert(connectionsCsvFilePath, connectionsTable)
-readAndInsert(monitoringCsvFilePath, monitoringTable)
-readAndInsert(eventsCsvFilePath, eventsTable)
-readAndInsert(positionsCsvFilePath, positionsTable)
+                if dataTypes[i] == "string":
+                    insertDict[header[i]] = x
+                elif dataTypes[i] == "int":
+                    insertDict[header[i]] = int(x)
+                elif dataTypes[i] == "float":
+                    insertDict[header[i]] = float(x)
+                elif dataTypes[i] == "date":
+                    insertDict[header[i]] = stringToDate(x)
+                i += 1
+            table.insert(insertDict)
 
+def stringToDate(string):
+    i = 0
+    char = string[i]
 
+    year = ""
+    month = ""
+    day = ""
+    hour = ""
+    minute = ""
+    second = ""
+    while(char!= "-"):
+        year = year + char
+        i += 1
+        char = string[i]
+    i += 1
+    char = string[i]
+    while(char!= "-"):
+        month = month + char
+        i += 1
+        char = string[i]
+    i += 1
+    char = string[i]
+    while(char!= " "):
+        day = day + string[i]
+        i += 1
+        char = string [i]
+    i += 1
+    char = string[i]
+    while(char!= ":"):
+        hour = hour + string[i]
+        i += 1
+        char = string [i]
+    i += 1
+    char = string[i]
+    while(char!= ":"):
+        minute = minute + string[i]
+        i += 1
+        char = string [i]
+    i += 1
+    char = string[i]
+    while(i < string.__len__()):
+        second = second + string[i]
+        i += 1
+        if i < string.__len__():
+            char = string [i]
+    date = datetime.datetime(int(year), int(month), int(day),
+                             int(hour), int(minute), int(second))
+    return date
+
+def insertCommand():
+    print('connections')
+    fastInsert(connectionsCsvFilePath, ";", connectionsTable, connectionsDataTypes)
+    print('monitoring')
+    fastInsert(monitoringCsvFilePath, ";", monitoringTable, monitoringDataTypes)
+    print('events')
+    fastInsert(eventsCsvFilePath, ";", eventsTable, eventsDataTypes)
+    print('positions')
+    fastInsert(positionsCsvFilePath, ";", positionsTable, positionsDataTypes)
+    print('done')
+
+def printDocuments():
+    cursor = db.connections.find()
+
+    for document in cursor:
+        print(document)
